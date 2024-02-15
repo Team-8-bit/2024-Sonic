0c30dfd20d36497b19c0c1426747fae1b620804d
Nolan Locke <nolan.s.locke@gmail.com>
2024-02-14 19:38:12 -0700

    cleanup / formatting

--- robot/src/main/kotlin/org/team9432/Robot.kt
+++ robot/src/main/kotlin/org/team9432/Robot.kt
@@ -11,22 +11,9 @@ import org.littletonrobotics.junction.wpilog.WPILOGReader
 import org.littletonrobotics.junction.wpilog.WPILOGWriter
 
 
-/**
- * The VM is configured to automatically run this object (which basically functions as a singleton class),
- * and to call the functions corresponding to each mode, as described in the TimedRobot documentation.
- * This is written as an object rather than a class since there should only ever be a single instance, and
- * it cannot take any constructor arguments. This makes it a natural fit to be an object in Kotlin.
- *
- * If you change the name of this object or its package after creating this project, you must also update
- * the `Main.kt` file in the project. (If you use the IDE's Rename or Move refactorings when renaming the
- * object or package, it will get changed everywhere.)
- */
 object Robot: LoggedRobot() {
-    private var autonomousCommand: Command? = null
 
     override fun robotInit() {
-        // Access the RobotContainer object so that it is initialized. This will perform all our
-        // button bindings, and put our autonomous chooser on the dashboard.
         Logger.recordMetadata("ProjectName", "2024 - Sonic") // Set a metadata value
 
         if (isReal()) {
@@ -43,54 +30,15 @@ object Robot: LoggedRobot() {
 
         // Logger.disableDeterministicTimestamps() // See "Deterministic Timestamps" in the "Understanding Data Flow" page
         Logger.start()
+
         RobotContainer
     }
-
 
     override fun robotPeriodic() {
         CommandScheduler.getInstance().run()
     }
 
-    override fun disabledInit() {
-
-    }
-
-    override fun disabledPeriodic() {
-
-    }
-
-    override fun autonomousInit() {
-        autonomousCommand = RobotContainer.getAutonomousCommand()
-        autonomousCommand?.schedule()
-    }
-
-    override fun autonomousPeriodic() {
-
-    }
-
-    override fun teleopInit() {
-        autonomousCommand?.cancel()
-    }
-
-    /** This method is called periodically during operator control.  */
-    override fun teleopPeriodic() {
-
-    }
-
     override fun testInit() {
-        // Cancels all running commands at the start of test mode.
         CommandScheduler.getInstance().cancelAll()
-    }
-
-    override fun testPeriodic() {
-
-    }
-
-    override fun simulationInit() {
-
-    }
-
-    override fun simulationPeriodic() {
-
     }
 }
