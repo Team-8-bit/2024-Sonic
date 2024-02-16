cebfc6bca7838af6d72b918dbf1080346d2abdc7
brudderbot <brudderbot@outlook.com>
2024-02-16 14:12:03 -0700

    Add Hopper

--- robot/src/main/kotlin/org/team9432/Ports.kt
+++ robot/src/main/kotlin/org/team9432/Ports.kt
@@ -5,4 +5,9 @@ object Ports {
         const val MOTOR_ID = 1
         const val LIMIT_ID = 1
     }
+    object Hopper {
+        const val MOTOR_ID = 1
+        const val Amp_BRAKE_ID = 1
+        const val Shooter_BRAKE_ID = 1
+    }
 }
---
+++ robot/src/main/kotlin/org/team9432/subsystems/hopper/HopperIO.kt
@@ -0,0 +1,14 @@
+package org.team9432.subsystems.hopper
+
+import org.team9432.lib.annotation.Logged
+
+interface HopperIO {
+    @Logged
+    open class HopperIOInputs {
+        var atAmpBeamBrake = false
+        var atShooterBeamBrake = false
+    }
+
+    fun updateInputs(inputs: HopperIOInputs)
+    fun setSpeed(speed: Double)
+}
