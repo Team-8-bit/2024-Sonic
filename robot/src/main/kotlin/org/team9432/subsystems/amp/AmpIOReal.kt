209942dde917c8cebd394c8c073f4ffa7e635736
brudderbot <brudderbot@outlook.com>
2024-02-16 13:46:57 -0700

    Remove unused import

--- robot/src/main/kotlin/org/team9432/subsystems/amp/AmpIOReal.kt
+++ robot/src/main/kotlin/org/team9432/subsystems/amp/AmpIOReal.kt
@@ -4,7 +4,6 @@ import com.revrobotics.CANSparkLowLevel
 import com.revrobotics.CANSparkMax
 import edu.wpi.first.wpilibj2.command.SubsystemBase
 import org.team9432.Ports
-import org.team9432.subsystems.hood.HoodIO
 
 class AmpIOReal: AmpIO, SubsystemBase() {
     private val spark = CANSparkMax(Ports.Amp.MOTOR_ID, CANSparkLowLevel.MotorType.kBrushless)
