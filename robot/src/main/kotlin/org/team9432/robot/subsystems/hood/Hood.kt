9bf97e207297d7400f1212a9572a1cf6c3076c31
nolan <nolan.s.locke@gmail.com>
2024-02-19 22:51:31 -0700

    actually implement hood

---
+++ robot/src/main/kotlin/org/team9432/robot/subsystems/hood/Hood.kt
@@ -0,0 +1,25 @@
+package org.team9432.robot.subsystems.hood
+
+import org.littletonrobotics.junction.Logger
+import org.team9432.Robot
+import org.team9432.Robot.Mode.*
+import org.team9432.lib.commandbased.KSubsystem
+
+object Hood: KSubsystem() {
+    private val inputs = LoggedHoodIOInputs()
+    private val io = when (Robot.mode) {
+        REAL, REPLAY -> HoodIOReal()
+        SIM -> TODO()
+    }
+
+    private var currentAngleTarget = 0.0
+        set(value) {
+            io.setAngle(value)
+        }
+
+    override fun constantPeriodic() {
+        io.updateInputs(inputs)
+        Logger.processInputs("Hood", inputs)
+        
+    }
+}
