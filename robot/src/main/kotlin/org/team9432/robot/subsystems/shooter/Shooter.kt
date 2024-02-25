8985f5f01bb475f293eae7bc0857724b66ed21ac
nolan <nolan.s.locke@gmail.com>
2024-02-25 16:21:07 -0700

    split shooter subsystem

--- robot/src/main/kotlin/org/team9432/robot/subsystems/shooter/Shooter.kt
+++ robot/src/main/kotlin/org/team9432/robot/subsystems/shooter/Shooter.kt
@@ -1,48 +1,28 @@
 package org.team9432.robot.subsystems.shooter
 
-import edu.wpi.first.math.controller.SimpleMotorFeedforward
-import org.littletonrobotics.junction.Logger
-import org.team9432.Robot
-import org.team9432.Robot.Mode.*
 import org.team9432.lib.commandbased.KSubsystem
 
 object Shooter: KSubsystem() {
-    private val io: ShooterIO
-    private val inputs = LoggedShooterIOInputs()
-
-    private val feedforward: SimpleMotorFeedforward
-
-    init {
-        when (Robot.mode) {
-            REAL, REPLAY -> {
-                io = ShooterIOVortex()
-                io.setPID(0.0, 0.0, 0.0)
-                feedforward = SimpleMotorFeedforward(0.0, 0.0)
-            }
-
-            SIM -> {
-                io = ShooterIOSim()
-                io.setPID(0.1, 0.0, 0.0)
-                feedforward = SimpleMotorFeedforward(0.0, 0.0)
-            }
-        }
-    }
+    private val leftSide = ShooterSide(ShooterSideIO.ShooterSide.LEFT)
+    private val rightSide = ShooterSide(ShooterSideIO.ShooterSide.RIGHT)
 
     override fun constantPeriodic() {
-        io.updateInputs(inputs)
-        Logger.processInputs("Shooter", inputs)
+        leftSide.periodic()
+        rightSide.periodic()
     }
 
     fun setVolts(leftVolts: Double, rightVolts: Double) {
-        io.setVoltage(leftVolts, rightVolts)
+        leftSide.setVolts(leftVolts)
+        rightSide.setVolts(rightVolts)
     }
 
     fun setSpeed(leftRPM: Double, rightRPM: Double) {
-        io.setSpeed(leftRPM, feedforward.calculate(leftRPM), rightRPM, feedforward.calculate(rightRPM))
-
-        Logger.recordOutput("Shooter/LeftSetpointRPM", leftRPM)
-        Logger.recordOutput("Shooter/RightSetpointRPM", rightRPM)
+        leftSide.setSpeed(leftRPM)
+        rightSide.setSpeed(rightRPM)
     }
 
-    fun stop() = io.stop()
+    fun stop() {
+        leftSide.stop()
+        rightSide.stop()
+    }
 }
