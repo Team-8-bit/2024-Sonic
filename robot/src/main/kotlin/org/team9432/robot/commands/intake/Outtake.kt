15c3756d5eb4580c6eeb415c6ed474d3b0a37345
nolan <nolan.s.locke@gmail.com>
2024-03-04 11:42:58 -0700

    update note position in the outtake command

--- robot/src/main/kotlin/org/team9432/robot/commands/intake/Outtake.kt
+++ robot/src/main/kotlin/org/team9432/robot/commands/intake/Outtake.kt
@@ -1,15 +1,14 @@
 package org.team9432.robot.commands.intake
 
-import org.team9432.lib.commandbased.commands.*
-import org.team9432.robot.MechanismSide
+import org.team9432.lib.commandbased.commands.SimpleCommand
 import org.team9432.robot.RobotState
-import org.team9432.robot.subsystems.beambreaks.BeambreakIOSim
-import org.team9432.robot.subsystems.hopper.CommandHopper
-import org.team9432.robot.subsystems.intake.CommandIntake
 import org.team9432.robot.subsystems.intake.Intake
 
 fun Outtake() = SimpleCommand(
     requirements = setOf(Intake),
     initialize = { Intake.outtake(8.0, 8.0) },
-    end = { Intake.stop() }
+    end = {
+        Intake.stop()
+        if (!RobotState.noteInAnyIntake()) RobotState.notePosition = RobotState.NotePosition.NONE
+    }
 )
