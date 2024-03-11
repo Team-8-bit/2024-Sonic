1d3112b64c7da12df5c00883f008cb29e8055f5d
nolan <nolan.s.locke@gmail.com>
2024-03-11 00:13:14 -0700

    remove amp auto scoring

--- robot/src/main/kotlin/org/team9432/robot/FieldConstants.kt
+++ robot/src/main/kotlin/org/team9432/robot/FieldConstants.kt
@@ -10,18 +10,6 @@ object FieldConstants {
     private val blueSpeakerPose = Pose2d(0.35, 5.40, Rotation2d())
     private val redSpeakerPose = PoseUtil.flip(blueSpeakerPose)
 
-    private val blueAmpPose = Pose2d(1.86, 7.78, Rotation2d.fromDegrees(-90.0))
-    private val redAmpPose = PoseUtil.flip(blueAmpPose)
-
-    private val redAmpAlignPose = Pose2d(14.675, 7.532, Rotation2d.fromDegrees(-90.0))
-    private val blueAmpAlignPose = PoseUtil.flip(redAmpAlignPose)
-
     val speakerPose
         get() = if (Robot.alliance == DriverStation.Alliance.Blue) blueSpeakerPose else redSpeakerPose
-
-    val ampPose
-        get() = if (Robot.alliance == DriverStation.Alliance.Blue) blueAmpPose else redAmpPose
-
-    val ampAlignPose
-        get() = if (Robot.alliance == DriverStation.Alliance.Blue) blueAmpAlignPose else redAmpAlignPose
 }
