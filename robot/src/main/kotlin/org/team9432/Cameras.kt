a5647acf427f6da63010af78800a7eaa706b59e7
nolan <nolan.s.locke@gmail.com>
2024-03-13 23:34:14 -0700

    potential camera fix

--- robot/src/main/kotlin/org/team9432/Cameras.kt
+++ robot/src/main/kotlin/org/team9432/Cameras.kt
@@ -1,65 +1,58 @@
 package org.team9432
 
 import edu.wpi.first.cameraserver.CameraServer
-import edu.wpi.first.cscore.CvSink
-import edu.wpi.first.cscore.CvSource
 import edu.wpi.first.cscore.UsbCamera
-import org.opencv.core.Mat
-import org.team9432.lib.commandbased.KSubsystem
 import org.team9432.robot.MechanismSide
 import org.team9432.robot.RobotState
 import org.team9432.robot.subsystems.drivetrain.Drivetrain
 import kotlin.math.abs
 
-object Cameras : KSubsystem() {
-    private var currentCamera = MechanismSide.AMP
-
-    private val ampCam: UsbCamera = CameraServer.startAutomaticCapture(1)
-    private val speakerCam: UsbCamera = CameraServer.startAutomaticCapture(0)
-
-    private val ampSink: CvSink = CameraServer.getVideo(ampCam)
-    private val speakerSink: CvSink = CameraServer.getVideo(speakerCam)
-    // private val outputStream: CvSource = CameraServer.putVideo("Driver Cam", 320 / 2, 240 / 2)
-
+// https://www.chiefdelphi.com/t/switchable-camera/458565/5
+object Cameras {
     init {
-        ampCam.setResolution(320 / 4, 240 / 4)
-        ampCam.setFPS(8)
-        speakerCam.setResolution(320 / 4, 240 / 4)
-        speakerCam.setFPS(8)
-    }
+        val thread = Thread {
+            var currentCamera = MechanismSide.AMP
 
-    private fun selectCamera(): MechanismSide {
-        // Only return a new camera when the robot is actually going
-        val speeds = Drivetrain.getSpeeds()
+            val ampCam = CameraServer.startAutomaticCapture(1)
+            val speakerCam = CameraServer.startAutomaticCapture(0)
 
-        return if (maxOf(abs(speeds.vxMetersPerSecond), abs(speeds.vyMetersPerSecond)) > 0.5) {
-            RobotState.getMovementDirection()
-        } else {
-            currentCamera
-        }
-    }
+            val server = CameraServer.getServer()
 
-    override fun periodic() {
-        currentCamera = selectCamera()
+            fun selectCamera(): MechanismSide {
+                // Only return a new camera when the robot is actually going
+                val speeds = Drivetrain.getSpeeds()
 
-        when (currentCamera) {
-            MechanismSide.AMP -> {
-                speakerSink.setEnabled(false)
-                ampSink.setEnabled(true)
+                return if (maxOf(abs(speeds.vxMetersPerSecond), abs(speeds.vyMetersPerSecond)) > 0.5) {
+                    RobotState.getMovementDirection()
+                } else {
+                    currentCamera
+                }
             }
 
-            MechanismSide.SPEAKER -> {
-                ampSink.setEnabled(false)
-                speakerSink.setEnabled(true)
+            server.source = ampCam
+
+            var lastCamera = currentCamera
+
+            while (!Thread.interrupted()) {
+                currentCamera = selectCamera()
+
+                if (currentCamera != lastCamera){
+                    lastCamera = currentCamera
+
+                    // Disable and wait
+                    server.source = null
+                    Thread.sleep(500)
+
+                    // Set the new camera
+                    when (currentCamera) {
+                        MechanismSide.AMP -> server.source = ampCam
+                        MechanismSide.SPEAKER -> server.source = speakerCam
+                    }
+                }
+                Thread.sleep(20)
             }
         }
-//
-//        val image = Mat()
-//
-//        when (currentCamera) {
-//            MechanismSide.AMP -> ampSink.grabFrame(image)
-//            MechanismSide.SPEAKER -> speakerSink.grabFrame(image)
-//        }
-        //outputStream.putFrame(image)
+
+        thread.start()
     }
 }
