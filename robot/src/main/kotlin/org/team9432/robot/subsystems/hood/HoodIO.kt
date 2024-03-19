854ecfaf506bc2e425dae0c0f3165b52ce087bd4
nolan <nolan.s.locke@gmail.com>
2024-03-19 13:59:44 -0700

    change hood to use the relative encoder

--- robot/src/main/kotlin/org/team9432/robot/subsystems/hood/HoodIO.kt
+++ robot/src/main/kotlin/org/team9432/robot/subsystems/hood/HoodIO.kt
@@ -6,11 +6,10 @@ import org.team9432.lib.annotation.Logged
 interface HoodIO {
     @Logged
     open class HoodIOInputs {
-        var absoluteAngle = Rotation2d()
-        var relativeAngle = Rotation2d()
+        var angle = Rotation2d()
         var appliedVolts = 0.0
         var currentAmps = 0.0
-        var velocityDegPerSec = 0.0
+        var velocityRadPerSec = 0.0
     }
 
     fun updateInputs(inputs: HoodIOInputs) {}
