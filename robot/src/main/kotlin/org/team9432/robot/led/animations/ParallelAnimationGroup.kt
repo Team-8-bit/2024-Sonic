2b2b05de34ce1bdd669e953e704ffa2a62bd5d14
nolan <nolan.s.locke@gmail.com>
2024-04-04 16:54:26 -0700

    call end() in parallel animations

--- robot/src/main/kotlin/org/team9432/robot/led/animations/ParallelAnimationGroup.kt
+++ robot/src/main/kotlin/org/team9432/robot/led/animations/ParallelAnimationGroup.kt
@@ -3,18 +3,19 @@ package org.team9432.robot.led.animations
 class ParallelAnimationGroup(vararg animations: Animation): Animation {
     private val animations = animations.toMutableList()
 
+    override fun start() {
+        animations.forEach { it.start() }
+    }
+
     override fun update(): Boolean {
         val finishedAnimations = mutableListOf<Animation>()
         for (animation in animations) {
             val isFinished = animation.update()
             if (isFinished) finishedAnimations.add(animation)
         }
+        finishedAnimations.forEach { it.end() }
         animations.removeAll(finishedAnimations)
 
         return animations.isEmpty()
-    }
-
-    override fun start() {
-        animations.forEach { it.start() }
     }
 }
