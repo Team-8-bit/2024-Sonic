36ba5ec1c740a078b3fd5b27ebe72f5e85b234b9
nolan <nolan.s.locke@gmail.com>
2024-04-12 12:05:49 -0700

    comment out the sysid controller

--- robot/src/main/kotlin/org/team9432/Robot.kt
+++ robot/src/main/kotlin/org/team9432/Robot.kt
@@ -1,6 +1,5 @@
 package org.team9432
 
-import edu.wpi.first.wpilibj2.command.button.CommandXboxController
 import org.littletonrobotics.junction.LoggedRobot
 import org.team9432.lib.commandbased.KCommandScheduler
 import org.team9432.lib.commandbased.commands.InstantCommand
@@ -10,11 +9,9 @@ import org.team9432.lib.commandbased.commands.withTimeout
 import org.team9432.robot.RobotState
 import org.team9432.robot.auto.AutoChooser
 import org.team9432.robot.auto.commands.PullFromSpeakerShooter
-import org.team9432.robot.commands.DefaultCommands
 import org.team9432.robot.commands.stop
 import org.team9432.robot.subsystems.Hood
 import org.team9432.robot.subsystems.Shooter
-import edu.wpi.first.wpilibj2.command.CommandScheduler as WPICommandScheduler
 
 val LOOP_PERIOD_SECS = Robot.period
 
@@ -47,36 +44,37 @@ object Robot: LoggedRobot() {
     override fun disabledExit() {
         hasBeenEnabled = true
     }
+    /*
+        init {
+            val controller = CommandXboxController(4)
+            val tests = Hood.getSysIdTests()
+            controller.a().whileTrue(tests.dynamicForward)
+            controller.b().whileTrue(tests.dynamicReverse)
+            controller.x().whileTrue(tests.quasistaticForward)
+            controller.y().whileTrue(tests.quasistaticReverse)
+        }
 
-    init {
-        val controller = CommandXboxController(4)
-        val tests = Hood.getSysIdTests()
-        controller.a().whileTrue(tests.dynamicForward)
-        controller.b().whileTrue(tests.dynamicReverse)
-        controller.x().whileTrue(tests.quasistaticForward)
-        controller.y().whileTrue(tests.quasistaticReverse)
-    }
+        // Use the wpilib command scheduler while in test mode for sysid
+        override fun testInit() {
+            KCommandScheduler.disable()
+            DefaultCommands.clearDefaultCommands()
+            KCommandScheduler.enable()
 
-    // Use the wpilib command scheduler while in test mode for sysid
-    override fun testInit() {
-        KCommandScheduler.disable()
-        DefaultCommands.clearDefaultCommands()
-        KCommandScheduler.enable()
+            KCommandScheduler.cancelAll()
 
-        KCommandScheduler.cancelAll()
+            WPICommandScheduler.getInstance().enable()
+        }
 
-        WPICommandScheduler.getInstance().enable()
-    }
+        override fun testPeriodic() {
+            WPICommandScheduler.getInstance().run()
+        }
 
-    override fun testPeriodic() {
-        WPICommandScheduler.getInstance().run()
-    }
+        override fun testExit() {
+            WPICommandScheduler.getInstance().disable()
 
-    override fun testExit() {
-        WPICommandScheduler.getInstance().disable()
-
-        KCommandScheduler.disable()
-        DefaultCommands.setDefaultCommands()
-        KCommandScheduler.enable()
-    }
+            KCommandScheduler.disable()
+            DefaultCommands.setDefaultCommands()
+            KCommandScheduler.enable()
+        }
+    */
 }
