b186c75f7bed212679c8ca437d15f04a195230f0
nolan <nolan.s.locke@gmail.com>
2024-05-05 16:56:25 -0700

    remove dashboard code

--- dashboard/dashboard-app/src/main/kotlin/org/team9432/dashboard/app/ui/theme/Type.kt
+++
@@ -1,5 +0,0 @@
-package org.team9432.dashboard.app.ui.theme
-
-import androidx.compose.material3.Typography
-
-val AppTypography = Typography()
--- dashboard/dashboard-shared/src/main/kotlin/org/team9432/dashboard/shared/Sendable.kt
+++
@@ -1,7 +0,0 @@
-package org.team9432.dashboard.shared
-
-import kotlinx.serialization.Serializable
-
-/** An interface representing any piece of information being moved between the robot and the dashboard. */
-@Serializable
-sealed interface Sendable
