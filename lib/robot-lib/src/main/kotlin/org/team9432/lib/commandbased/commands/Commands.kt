7defa88747c16b71b09dfc4e5c802e388a663e09
nolan <nolan.s.locke@gmail.com>
2024-05-06 12:59:47 -0700

    remove library code

--- lib/robot-lib/src/main/kotlin/org/team9432/lib/commandbased/commands/Commands.kt
+++
@@ -1,9 +0,0 @@
-package org.team9432.lib.commandbased.commands
-
-import org.team9432.lib.commandbased.KSubsystem
-
-/** A Command that prints a given string when initialized. */
-fun PrintCommand(message: String) = InstantCommand { println(message) }
-
-/** A Command that runs instantly; it will initialize, execute once, and end on the same iteration of the scheduler. */
-fun InstantCommand(vararg requirements: KSubsystem, runnable: () -> Unit) = SimpleCommand(initialize = runnable, isFinished = { true }, requirements = requirements.toSet())
--- lib/robot-lib/src/main/kotlin/org/team9432/lib/wrappers/cancoder/LoggedCancoderIOSim.kt
+++
@@ -1,7 +0,0 @@
-package org.team9432.lib.wrappers.cancoder
-
-class LoggedCancoderIOSim(val config: LoggedCancoder.Config): LoggedCancoderIO {
-    override fun updateInputs(inputs: LoggedCancoderIO.CancoderIOInputs) {
-        inputs.position = config.simPositionSupplier.invoke()
-    }
-}
